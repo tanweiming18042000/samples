33c0e8f68abde7209abba0e0f5f53d79ca8aeb2c
tanweiming18042000 <tanweiming18042000@gmail.com>
2022-03-11 19:16:05 +0800

    Third commit

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,23 @@
+package samples.demo;
+
+public class Location {
+	private String name;
+	private String address;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String address() {
+		return address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+}
